402b5f6b229815f3eb2c093d1b96bcdb38c0ab9d
Arbol <tringuy1993@gmail.com>
2023-07-04 10:28:33 -0700

    added small change for urls.py

--- btbackend/urls.py
+++ btbackend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('backtest-data/', include('mainapp.urls'))
+    path('api/backtest-data/', include('mainapp.urls'))
 ]
